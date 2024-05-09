cfa84e082a95d4679ff63c1912b627662c13af73
Carlos <carlossanchez170204@gmail.com>
2024-05-09 13:16:25 +0200

    He agregado enlaces a los proyectos

---
+++ js/enlaces.js
@@ -0,0 +1,10 @@
+const $tiendaOnline = document.getElementById("tienda-online");
+const $frontEndMentor = document.getElementById("frontend-mentor");
+
+$tiendaOnline.addEventListener("click", ()=>{    
+    window.location.href = "https://camisesp.evirom.com/";
+})
+
+$frontEndMentor.addEventListener("click", ()=>{
+    window.location.href = "https://challenges-frontend-mentor-three.vercel.app/";
+})
