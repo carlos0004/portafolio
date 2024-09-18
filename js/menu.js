8f525d80fa8ff85a7716e6084e430cdb38beef3a
carlos0004 <carlossanchez170204@gmail.com>
2024-09-18 21:28:58 +0200

    arreglo menu móvil

--- js/menu.js
+++ js/menu.js
@@ -1,47 +1,22 @@
-// // nav a {
-// //     display: block;
-// // }
-// // header nav 
-// .header-nav{
-//     display: flex;
-//     flex-direction: column;
-//     height: 100dvh;
-//     padding-top: 20%;
-//     gap: 10%;
-// }
-
-// // .menu a 
-// .menu-a{
-//     display: flex;
-//     justify-content: center;
-//     padding: 10px;
-//     width: 100%;
-//     position: relative;
-// }
-
-// .menu a::after {
-//     content: "";
-//     position: absolute;
-//     bottom: 0;
-//     left: 0;
-//     width: 100%;
-//     height: 2px;
-//     background-image: linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(255, 255, 255, 1) 49%, rgba(0, 0, 0, 1) 100%);
-// }
-
-const $abrirCerrarMenu = document.querySelector(".abrir-cerrar");
+const $abrirCerrarMenu = document.querySelector(".abrir-cerrar input");
 const $headerNav = document.querySelector("header nav");
 const $links = document.querySelectorAll("nav a");
 
-$abrirCerrarMenu.getElementsByTagName("input")[0].addEventListener("click", (e) => {
+$abrirCerrarMenu.addEventListener("click", (e) => {
+    if ($abrirCerrarMenu.checked) {
+        document.body.style.overflow = "hidden"; // Desactiva el scroll cuando el menú está abierto
+        $headerNav.classList.add("active"); // Añade la clase 'active' al nav
+    } else {
+        document.body.style.overflow = "auto"; // Activa el scroll cuando el menú se cierra
+        $headerNav.classList.remove("active");
+    }
+});
 
-    document.body.style.overflow = "hidden";
-    $headerNav.classList.toggle("active");
-
-    $links.forEach(link => {
-        link.addEventListener("click", () => {
-            document.body.style.overflow = "auto";
-            $headerNav.classList.remove("active");
-        })
-    })
+// Cerrar el menú al hacer clic en cualquier enlace
+$links.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.style.overflow = "auto"; // Activa el scroll
+        $headerNav.classList.remove("active"); // Remueve la clase 'active' del nav
+        $abrirCerrarMenu.checked = false; // Desactiva el checkbox, lo que cierra el menú
+    });
 });
